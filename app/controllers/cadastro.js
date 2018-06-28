131027cc5e934f29d70407cda93027ce2e5399e5
RAFAEL VALE <RAFAEL.VALE@RCKY.local>
2018-06-28 17:39:57 -0300

    inclusão de rotas e controllers

---
+++ app/controllers/cadastro.js
@@ -0,0 +1,25 @@
+module.exports.cadastro = function(application, req, res){
+    res.render('cadastro', {validacao: {}, dadosForm: {}});
+}
+
+module.exports.cadastrar = function(application, req, res){
+    var dadosForm = req.body;
+
+    req.assert('nome', 'nome não pode ser vazio').notEmpty();
+    req.assert('usuario', 'usuario não pode ser vazio').notEmpty();
+    req.assert('senha', 'senha não pode ser vazio').notEmpty();
+    req.assert('casa', 'casa não pode ser vazio').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('cadastro', {validacao: erros, dadosForm: dadosForm });
+        console.log(erros)
+        return;
+    }
+        res.send('')
+
+
+
+
+}
